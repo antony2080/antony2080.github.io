93d6d36ecfc3295327552979ef55bfa8e6a8f0bd
antony2080 <50128277+antony2080@users.noreply.github.com>
2024-03-27 14:49:05 +0800

    Copy 404 page

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from "url"
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: './',
   plugins: [vue()],
   resolve: {
     alias: {
