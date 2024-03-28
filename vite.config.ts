8966e794d4ae161a60b4075370759b3d0aba756d
antony2080 <50128277+antony2080@users.noreply.github.com>
2024-03-28 10:09:58 +0800

    Add local https plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import mkcert from 'vite-plugin-mkcert'
 // @ts-ignore
 import { fileURLToPath, URL } from "url"
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [vue()],
+  plugins: [vue(), mkcert()],
+  server: {
+    https: true
+  },
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url))
