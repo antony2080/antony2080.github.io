1ba354ce26eda5639438299505eba07bfd1af490
antony2080 <50128277+antony2080@users.noreply.github.com>
2024-03-29 16:15:20 +0800

    Use isAuthenticated to control UI

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import { useUserStore } from '@/stores/user'
+import { storeToRefs } from 'pinia';
 const store = useUserStore()
+const { isAuthenticated } = storeToRefs(store)
 store.load()
 </script>
 
 <template>
-  <div class="drawer drawer-open">
+  <div class="drawer md:drawer-open">
     <input type="checkbox" id="my-drawer-2" class="drawer-toggle">
     <div class="drawer-content">
       <router-view></router-view>
@@ -14,7 +16,7 @@ store.load()
       <label for="my-drawer-2" aria-label="close sidebar" class="drawer-overlay"></label>
       <ul class="menu w-80 min-h-full p-4 bg-base-100">
         <li><router-link to="/">Home</router-link></li>
-        <li><router-link to="/login">Login</router-link></li>
+        <li v-if="!isAuthenticated"><router-link to="/login">Login</router-link></li>
         <li><router-link to="/404">404</router-link></li>
       </ul>
     </div>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,12 +1,30 @@
 <script setup lang="ts">
 import { useUserStore } from '@/stores/user'
 import { storeToRefs } from 'pinia';
-import { onMounted } from 'vue';
+import { onMounted, ref } from 'vue';
+import { useRouter } from 'vue-router'
 const store = useUserStore()
+const { isAuthenticated } = storeToRefs(store)
+const router = useRouter()
+
 const { profile } = storeToRefs(store)
+const loading = ref(false)
+
+const doGetMe = () => {
+  setTimeout(async () => {
+    loading.value = true
+    await store.getMe()
+    loading.value = false
+  }, 100)
+}
+
+const logout = () => {
+  store.logout()
+  router.push('/login')
+}
 
 onMounted(() => {
-  setTimeout(() => { store.getMe() }, 200)
+  doGetMe()
 })
 </script>
 
@@ -15,7 +33,8 @@ onMounted(() => {
     <h1 class="text-3xl mb-4">Home</h1>
     <div class="card bg-white shadow-md">
       <div class="card-body">
-        <div class="flex items-center">
+        <span class="loading loading-spinner loading-lg text-primary" v-if="loading"></span>
+        <div class="flex items-center" v-else>
           <div class="avatar">
             <div class="w-12 rounded">
               <img :src="profile.url" alt="me">
@@ -25,6 +44,14 @@ onMounted(() => {
             <h1 class="text-xl font-bold">{{ profile.name }}</h1>
             <p class="text-slate-500">{{ profile.email }}</p>
           </div>
+        </div>
+        <div class="card-actions justify-end" v-if="isAuthenticated">
+          <button class="btn bg-purple-600 hover:bg-purple-500 text-white" @click="logout"><svg fill="currentColor"
+              class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+              <title>facebook</title>
+              <path
+                d="M12 2.04C6.5 2.04 2 6.53 2 12.06C2 17.06 5.66 21.21 10.44 21.96V14.96H7.9V12.06H10.44V9.85C10.44 7.34 11.93 5.96 14.22 5.96C15.31 5.96 16.45 6.15 16.45 6.15V8.62H15.19C13.95 8.62 13.56 9.39 13.56 10.18V12.06H16.34L15.89 14.96H13.56V21.96A10 10 0 0 0 22 12.06C22 6.53 17.5 2.04 12 2.04Z" />
+            </svg>Logout</button>
         </div>
       </div>
     </div>
