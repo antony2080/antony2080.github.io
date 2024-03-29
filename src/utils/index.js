8e2b304320fe469faacf2046dc2a93da00c59045
antony2080 <50128277+antony2080@users.noreply.github.com>
2024-03-29 15:03:10 +0800

    Separate fb-api from utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,66 +1,6 @@
-const appId = import.meta.env.VITE_FACEBOOK_APP_ID;
-
-function initFacebookSdk() {
-  return new Promise(resolve => {
-    // wait for facebook sdk to initialize before starting the vue app
-    window.fbAsyncInit = function () {
-      FB.init({
-        appId: appId,
-        cookie: true,
-        xfbml: true,
-        version: 'v19.0'
-      });
-
-      // auto authenticate with the api if already logged in with facebook
-      FB.getLoginStatus(({ authResponse }) => {
-        if (authResponse) {
-          console.log('[authResponse]:', JSON.stringify(authResponse));
-        } else {
-          resolve();
-        }
-      });
-    };
-
-    // load facebook sdk script
-    (function (d, s, id) {
-      var js, fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) { return; }
-      js = d.createElement(s); js.id = id;
-      js.src = "https://connect.facebook.net/en_US/sdk.js";
-      fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
-  });
-}
-
-
-const apiLogin = async () => {
-  let result = await new Promise((resolve) => {
-    FB.login(function (response) {
-      console.log(response);
-    }, { scope: 'email' });
-  });
-  return result;
-};
-
-const apiMe = async () => {
-  let result = await new Promise((resolve) => {
-    FB.api('/me', 'GET', { "fields": "id,name,email,picture" }, function (res) {
-      console.log(res);
-      resolve(res);
-    });
-  });
-  return result;
-};
-
-const apiLogout = () => {
-  FB.api('/me/permissions', 'delete', null, () => {
-    FB.logout();
-  });
-};
-
 export {
   initFacebookSdk,
   apiLogin,
   apiLogout,
   apiMe
-};
+} from './fb-api.js';
