36c1bcd80f649c1e7ce243295aae09db66592000
antony2080 <50128277+antony2080@users.noreply.github.com>
2024-04-08 13:56:26 +0800

    Add auto-toggled drawer

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,18 @@ import Navbar from '@/components/Navbar.vue'
 import { useUserStore } from '@/stores/user'
 import Toast from '@/components/Toast.vue'
 import { storeToRefs } from 'pinia';
+import { ref } from 'vue';
 const store = useUserStore()
+const isDrawerOpen = ref(false)
 const { isAuthenticated } = storeToRefs(store)
 store.load()
+
+const toggleDrawer = () => isDrawerOpen.value = !isDrawerOpen.value
 </script>
 
 <template>
   <div class="drawer md:drawer-open">
-    <input type="checkbox" id="my-drawer-2" class="drawer-toggle">
+    <input type="checkbox" id="my-drawer-2" class="drawer-toggle" v-model="isDrawerOpen">
     <div class="drawer-content">
       <Navbar />
       <router-view></router-view>
@@ -18,9 +22,9 @@ store.load()
     <div class="drawer-side">
       <label for="my-drawer-2" aria-label="close sidebar" class="drawer-overlay"></label>
       <ul class="menu w-80 min-h-full p-4 bg-base-100">
-        <li><router-link to="/">Home</router-link></li>
-        <li v-if="!isAuthenticated"><router-link to="/login">Login</router-link></li>
-        <li><router-link to="/404">404</router-link></li>
+        <li><router-link to="/" @click="toggleDrawer">Home</router-link></li>
+        <li v-if="!isAuthenticated"><router-link to="/login" @click="toggleDrawer">Login</router-link></li>
+        <li><router-link to="/404" @click="toggleDrawer">404</router-link></li>
       </ul>
     </div>
   </div>
