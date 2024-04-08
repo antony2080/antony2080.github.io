4433f70054068c2d628337dafaa752a77d400071
antony2080 <50128277+antony2080@users.noreply.github.com>
2024-04-08 10:50:14 +0800

    Remove fbInit
    Use load in user store

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,7 @@ import { createPinia } from 'pinia'
 import './style.css'
 import App from './App.vue'
 import { router } from '@/router/index'
-// @ts-ignore
-import { initFacebookSdk } from '@/utils/index'
 
-initFacebookSdk().then(startApp)
-
-function startApp() {
-  const app = createApp(App)
-  app.use(router).use(createPinia())
-  app.mount('#app')
-}
-startApp()
+const app = createApp(App)
+app.use(router).use(createPinia())
+app.mount('#app')
